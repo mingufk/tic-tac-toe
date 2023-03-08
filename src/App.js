afde8df86d120b53e76e7f354e19e9a4b07c72da
Mingu Kim <mingufk@gmail.com>
2023-03-09 02:25:03 +0900

    Building the board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Square from './components/Square';
+import Board from './components/Board';
 
 function App() {
   return (
     <>
-      <Square />
+      <Board />
     </>
   );
 }
